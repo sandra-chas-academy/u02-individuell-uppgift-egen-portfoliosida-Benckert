45ff8765873c3df6baabdf538b24060ea5c4fc51
Kristoffer <kristoffer.benckert@chasacademy.se>
2024-11-30 23:44:01 +0100

    adds toggle nav functionality

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,1 +1,13 @@
 // Main JS-file, import here
+const navToggle = document.getElementById("nav-toggle");
+const navLinks = document.getElementById("nav-links");
+
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("hidden");
+  updateToggleIcon();
+});
+
+function updateToggleIcon() {
+  navToggle.classList.toggle("fa-bars");
+  navToggle.classList.toggle("fa-xmark");
+}
